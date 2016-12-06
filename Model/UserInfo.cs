88aa4fc993137e550da10bdac35b0eae537c6278
zhangxiuzhen <Jianghao@APJ-EEL-JH013>
2016-12-06 13:31:25 +0800

    给UserInfo类添加注释

--- Model/UserInfo.cs
+++ Model/UserInfo.cs
@@ -16,19 +16,61 @@ namespace Model
         /// 用户账号
         /// </summary>
         public string UserAccount { get; set; }
+        /// <summary>
+        /// 用户名
+        /// </summary>
         public string UserName { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
         public string Password { get; set; }
+        /// <summary>
+        /// 班级Id
+        /// </summary>
         public int ClassId { get; set; }
+        /// <summary>
+        /// 有效日期
+        /// </summary>
         public DateTime? ExpireDate { get; set; }
+        /// <summary>
+        /// 标题
+        /// </summary>
         public string Title { get; set; }
+        /// <summary>
+        /// 部门
+        /// </summary>
         public string Department { get; set; }
+        /// <summary>
+        /// 邮箱
+        /// </summary>
         public string Email { get; set; }
+        /// <summary>
+        /// 电话
+        /// </summary>
         public string Phone { get; set; }
+        /// <summary>
+        /// 角色Id
+        /// </summary>
         public int RoleId { get; set; }
+        /// <summary>
+        /// 激活状态
+        /// </summary>
         public bool IsActive { get; set; }
+        /// <summary>
+        /// 创建者
+        /// </summary>
         public string CreatedBy { get; set; }
+        /// <summary>
+        /// 创建日期
+        /// </summary>
         public DateTime? CreatedDate { get; set; }
+        /// <summary>
+        /// 最后一次更新者
+        /// </summary>
         public string LastUpdatedBy { get; set; }
+        /// <summary>
+        /// 最后一次更新的日期
+        /// </summary>
         public DateTime? LastUpdatedDate { get; set; }
     }
 }
