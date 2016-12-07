630f3e1503cc363c717350574548640bf5a03c39
DENG <Administrator@PC201412152034>
2016-12-07 09:33:34 +0800

    添加了一个model类ViewLeaveManagement及相应的控制器和视图、分部页。。。

--- MVC_ITE/Controllers/LeaveManagementController.cs
+++ MVC_ITE/Controllers/LeaveManagementController.cs
@@ -12,12 +12,33 @@ namespace MVC_ITE.Controllers
     {
         //
         // GET: /LeaveManagement/
-        public ActionResult LeaveManagement()
+        /// <summary>
+        /// 显示主页信息
+        /// </summary>
+        /// <returns></returns>
+        
+        
+        public ActionResult LeaveManagement(int id = 1)
         {
-            UserInfoService userInfoService = new UserInfoService();
-            var list = userInfoService.GetUserInfo();
-            ViewData["list"] = list;
-            return View();
+            ViewLeaveManagementService viewleavelist = new ViewLeaveManagementService();
+            //获取已审批的请假单数据
+            var overList = viewleavelist.OverViewLeaveList();
+            //获取未审批的请假单数据
+            //var list = viewleavelist.ViewLeaveList();
+            ViewData["overList"] = overList;
+            //ViewData["list"] = list;
+            //return View();
+            int pageSize = 2;
+            //判断是否为Ajax请求
+            if (Request.IsAjaxRequest())
+            {
+                //加载用户列表分部视图
+                return PartialView("testController/_AjaxUserList", viewleavelist.ViewLeaveList(id, pageSize));
+            }
+            //将数据返回到前台
+            return View(viewleavelist.ViewLeaveList(id, pageSize));
         }
-	}
+        
+
+    }
 }
---
+++ Model/ViewLeaveManagement.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+    /// <summary>
+    /// 视图(表Class-表userinfo-表LeaveRequest-表SystemCode的联合查询)，获取请假条的信息
+    /// </summary>
+    public class ViewLeaveManagement
+    {
+        [Key]
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// 请假单编号
+        /// </summary>
+        public string LeaveNo { get; set; }
+        /// <summary>
+        /// 班级名称
+        /// </summary>
+        public string ClassName { get; set; }
+        /// <summary>
+        /// 用户姓名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+        public DateTime StartDateTime { get; set; }
+        /// <summary>
+        /// 结束时间
+        /// </summary>
+        public DateTime EndDateTime { get; set; }
+        /// <summary>
+        /// 请假天数
+        /// </summary>
+        public decimal LeaveDays { get; set; }
+        /// <summary>
+        /// 请假单状态ID
+        /// </summary>
+        public int LeaveStatusId { get; set; }
+        /// <summary>
+        /// 请假单状态
+        /// </summary>
+        public string CodeDescription { get; set; }
+        /// <summary>
+        /// 请假类型
+        /// </summary>
+        public string LeaveCategory { get; set; }
+        
+        
+    }
+}
